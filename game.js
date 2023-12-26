3adf904df84d06ad7f302ee8ba6e114a6876b5cf
cunzo <58085535+cunzo@users.noreply.github.com>
2023-12-26 16:23:37 +0100

    Creata una UI, gioco disponibile a schermo

--- game.js
+++ game.js
@@ -1,4 +1,21 @@
+let winCount = 0;
+let loseCount = 0; 
+let drawCount = 0;
+let totMatch = 0;
 const choice = ['ROCK','PAPER','SCISSOR'];
+const choiceRock = document.querySelector('#choiceRock');
+const choicePaper = document.querySelector('#choicePaper');
+const choiceScissor = document.querySelector('#choiceScissor');
+
+const result = document.querySelector('#result');
+const winCountEl = document.querySelector('#winCount');
+const loseCountEl = document.querySelector('#loseCount');
+const drawCountEl = document.querySelector('#drawCount');
+const totMatchEl = document.querySelector('#totMatch');
+
+choiceRock.addEventListener('click',play);
+choicePaper.addEventListener('click',play);
+choiceScissor.addEventListener('click',play);
 
 function getComputerChoice() {
     const randomizer = (min, max) => { return Math.round(Math.random() * (max - min) + min) };
@@ -7,40 +24,41 @@ function getComputerChoice() {
     return cpuChoice;
 }
 
-function getUserChoice() {
-    const userChoice = prompt('Hi make your choice (Rock - Paper - Scissor): ','paper');
-    console.log(`User choice: ${userChoice}`);
-    return userChoice;
-}
-
 function play() {
-    let winCount = 0;
-    let loseCount = 0;
-    let drawCount = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const userChoice = getUserChoice().toUpperCase();
-        const cpuChoice = getComputerChoice().toUpperCase();
-        if (!choice.includes(userChoice)) {
-            throw new Error('Invalid user choice');
-        } else if (userChoice == cpuChoice) {
-            drawCount += 1;
-            console.log(`You Draw!`);
-        } else if (
-            (userChoice == 'ROCK' && cpuChoice == 'SCISSOR') || 
-            (userChoice == 'PAPER' && cpuChoice == 'ROCK') || 
-            (userChoice == 'SCISSOR' && cpuChoice == 'PAPER')
-        ) { 
-            console.log(`You Win!`); 
-            winCount += 1;
-        } else {
-            console.log(`You Lose!`);
-            loseCount += 1;
-        }
+    totMatch += 1;
+    const userChoice = this.value.toUpperCase();
+    const cpuChoice = getComputerChoice().toUpperCase();
+    if (userChoice == cpuChoice) {
+        drawCount += 1;
+        result.textContent = `You Draw!`;
+    } else if (
+        (userChoice == 'ROCK' && cpuChoice == 'SCISSOR') || 
+        (userChoice == 'PAPER' && cpuChoice == 'ROCK') || 
+        (userChoice == 'SCISSOR' && cpuChoice == 'PAPER')
+    ) { 
+        result.textContent = `You Win!`;
+        winCount += 1;
+    } else {
+        result.textContent = `You Lose!`;
+        loseCount += 1;
     }
 
-    console.log(`Win count: ${winCount}`);
-    console.log(`Lose count: ${loseCount}`);
-    console.log(`Draw count: ${drawCount}`);
-
+    if (winCount == 5) {
+        result.textContent = `YOU WIN THE GAME!`;
+        winCount = 0;
+        loseCount = 0;
+        drawCount = 0;
+        totMatch = 0;
+    } else if (loseCount == 5) {
+        result.textContent = `YOU LOSE THE GAME!`;
+        winCount = 0;
+        loseCount = 0;
+        drawCount = 0;
+        totMatch = 0;
+    }
+    
+    winCountEl.textContent = `W:${winCount}`;
+    loseCountEl.textContent = `L:${loseCount}`;
+    drawCountEl.textContent = `D:${drawCount}`;
+    totMatchEl.textContent = `All match:${totMatch}`;
 }
